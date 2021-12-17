6820aff9a7dab7d5a90289f70c2ce5627cbb610e
rafael Pietri-Davila <rafael.pietri@gmail.com>
2021-12-17 18:34:48 +0100

    1st commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeHolder, handleChange }) => (
+    <input
+        className='search' 
+        type='search' 
+        placeholder={placeHolder}  
+        onChange={handleChange}
+    />
+);
